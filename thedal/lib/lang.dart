aceef058c9eabae0e2454c06d9845b735e20477f
Rakesh Roshan P <rakeshroshan.p2019cse@sece.ac.in>
2021-04-10 21:51:34 +0530

    new update

--- thedal/lib/lang.dart
+++ thedal/lib/lang.dart
@@ -11,153 +11,68 @@ class _LanguageState extends State<Language> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              children: [
-                Text(
-                  "Select Language",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * .7,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.transparent,
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                        ),
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        onPressed: () {
-                          setState(() {
-                            lang = "tamil";
-                          });
-                        },
-                        child: Card(
-                          child: Container(
-                              decoration: BoxDecoration(
-                                  gradient: LinearGradient(colors: [
-                                Color.fromRGBO(252, 92, 125, 1),
-                                Color.fromRGBO(106, 130, 251, 1),
-                              ])),
-                              width: 200,
-                              height: 100,
-                              alignment: Alignment.center,
-                              child: Image.asset("Asset/img/Tamil.png")),
-                        ),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.transparent,
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                        ),
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        onPressed: () {
-                          setState(() {
-                            lang = "english";
-                          });
-                        },
-                        child: Card(
-                          elevation: 5,
-                          child: Container(
-                              width: 200,
-                              height: 100,
-                              alignment: Alignment.center,
-                              child: Image.asset("Asset/img/English.png")),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      GestureDetector(
-                        child: Card(
-                          elevation: 5,
-                          child: Container(
-                              width: 200,
-                              height: 100,
-                              alignment: Alignment.center,
-                              child: Image.asset("Asset/img/japanese.png")),
-                        ),
-                      ),
-                      GestureDetector(
-                        child: Card(
-                          elevation: 5,
-                          child: Container(
-                            width: 200,
-                            height: 100,
-                            alignment: Alignment.center,
-                            child: Image.asset("Asset/img/chinese.png"),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        child: Card(
-                          elevation: 5,
-                          child: Container(
-                              width: 200,
-                              height: 100,
-                              alignment: Alignment.center,
-                              child: Image.asset("Asset/img/Telungu.png")),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => Company()));
-                  },
-                  child: Card(
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(30),
-                      ),
-                    ),
-                    elevation: 5,
-                    child: Container(
-                      width: 200,
-                      height: 70,
-                      alignment: Alignment.center,
-                      child: Text("Next"),
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(141, 91, 152, 1),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(4, 116, 191, 1),
+        leading: Container(
+          width: 330,
+          height: 100,
+          child: Image.asset("Asset/img/ThedalLogo.png"),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset("Asset/img/Line 1.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+                child: Text(
+              "Guest",
+              style: TextStyle(fontSize: 35),
+            )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset("Asset/img/GuestLogo.png"),
+          )
+        ],
+      ),
+      backgroundColor: Color.fromRGBO(138, 208, 236, 1),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                  child: Container(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                  child: Image.asset(
+                    "Asset/img/Cartoon.png",
+                    height: 245,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              )),
+              Expanded(
+                  child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          "Asset/img/language-solid.png",
+                          height: 362,
+                          width: 105,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ))
+            ],
+          )
+        ],
       ),
     );
   }
